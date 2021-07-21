b6c5de4398854c22e9536207df6ebee3a60a817d
leichen1 <lei5.chen@intel.com>
2021-07-21 15:17:21 +0800

    Build successfully.

--- mlir/examples/standalone/standalone-pipeline/standalone-pipeline.cpp
+++ mlir/examples/standalone/standalone-pipeline/standalone-pipeline.cpp
@@ -2,21 +2,20 @@
 // Created by leichen1 on 2021/7/15.
 //
 #include<iostream>
-//#include "standalone/Dialect.h"
 #include "standalone/MLIRGen.h"
 #include "standalone/Parser.h"
+#include "Standalone/Passes.h"
 
 #include "mlir/IR/AsmState.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
-#include "mlir/IR/Verifier.h"
 #include "mlir/Parser.h"
+#include "mlir/Transforms/Passes.h"
+#include "mlir/Pass/PassManager.h"
 
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ErrorOr.h"
 #include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "Standalone/StandaloneDialect.h"
@@ -36,12 +35,83 @@ std::unique_ptr<standalone::ModuleAST> parseInputFile(llvm::StringRef filename) 
     return parser.parseModule();
 }
 
+static char* inputFilename;
+
+int loadMLIR(mlir::MLIRContext &context, mlir::OwningModuleRef &module) {
+    // TAG: 1. Translate from .standalone to .ast and then to .mlir.
+    if(!llvm::StringRef(inputFilename).endswith(".mlir")) {
+        auto moduleAST = parseInputFile(inputFilename);
+        if (!moduleAST) {
+            return 6;
+        }
+        module = mlirGen(context, *moduleAST);
+        if (!module) {
+            return 1;
+        }
+        module->dump();
+        std::cout << "loadMLIR dump end" << std::endl;
+        return 0;
+    }
+    return 0;
+}
+
+int loadAndProcessMLIR(mlir::MLIRContext &context,
+                       mlir::OwningModuleRef &module) {
+    if (int error = loadMLIR(context, module)) {
+        return error;
+    }
+
+
+    // TAG: 2. Add pass.
+    mlir::PassManager pm(&context);
+    applyPassManagerCLOptions(pm);
+
+    // Pre-processing
+    pm.addPass(mlir::createInlinerPass());
+
+    //mlir::OpPassManager &optPM = pm.nest<mlir::FuncOp>();
+    //optPM.addPass(mlir::standalone::createShapeInferencePass());
+    //optPM.addPass(mlir::createCanonicalizerPass());
+    //optPM.addPass(mlir::createCSEPass());
+
+    // Lower to Linalg
+    /*
+    mlir::OpPassManager &optPM2 = pm.nest<mlir::FuncOp>();
+    optPM2.addPass(mlir::standalone::createLowerToLinalgPass());
+    optPM2.addPass(mlir::createCanonicalizerPass());
+    optPM2.addPass(mlir::createCSEPass());
+     */
+
+    // Lower to Affine
+    /*
+    mlir::OpPassManager &optPM2 = pm.nest<mlir::FuncOp>();
+    optPM2.addPass(mlir::standalone::createLowerToAffinePass());
+    optPM2.addPass(mlir::createCanonicalizerPass());
+    optPM2.addPass(mlir::createCSEPass());
+    optPM2.addPass(mlir::createLoopFusionPass());
+    optPM2.addPass(mlir::createAffineScalarReplacementPass());
+
+    // Lower to LLVM
+    pm.addPass(mlir::standalone::createLowerToLLVMPass());
+*/
+    if (mlir::failed(pm.run(*module)))
+        return 4;
+
+    module->dump();
+    std::cout << "loadAndProcessMLIR dump end" << std::endl;
+    return 0;
+}
+
 int main(int argc, char** argv) {
     std::cout << "Standalone pipeline" << std::endl;
     mlir::MLIRContext context;
     context.getOrLoadDialect<mlir::standalone::StandaloneDialect>();
 
-    if(llvm::StringRef(argv[1]).endswith(".mlir")) {
+    std::cout << argv[1] << std::endl;
+    inputFilename = argv[1];
+    /*
+    if(!llvm::StringRef(argv[1]).endswith(".mlir")) {
+        std::cout << "" << std::endl;
         auto moduleAST = parseInputFile(argv[1]);
         if (!moduleAST)
             return 6;
@@ -52,5 +122,12 @@ int main(int argc, char** argv) {
         module->dump();
         return 0;
     }
+    */
+
+    mlir::OwningModuleRef module;
+    if (int error = loadAndProcessMLIR(context, module)) {
+        return error;
+    }
+
     return 0;
 }
