387f2eea5fafb5f8f02625bc939d136a71bbe967
leichen1 <lei5.chen@intel.com>
2021-07-27 11:52:21 +0800

    Add files for "Add shape inference feature."

---
+++ mlir/examples/standalone/include/Standalone/Passes.h
@@ -0,0 +1,21 @@
+//
+// Created by leichen1 on 2021/7/19.
+//
+
+#ifndef STANDALONE_DIALECT_PASSES_H
+#define STANDALONE_DIALECT_PASSES_H
+
+#include <memory>
+
+namespace mlir {
+class Pass;
+namespace standalone {
+    std::unique_ptr<Pass> createShapeInferencePass();
+
+    //std::unique_ptr<mlir::Pass> createLowerToAffinePass();
+
+    //std::unique_ptr<mlir::Pass> createLowerToLLVMPass();
+}
+}
+
+#endif //STANDALONE_DIALECT_PASSES_H
